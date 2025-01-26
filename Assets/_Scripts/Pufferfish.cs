540202c6899e68824124b6443833ca288b4119ad
Joseph Seo <jseo305@gatech.edu>
2025-01-26 01:34:57 -0500

    Pufferfish Animations

--- Assets/_Scripts/Pufferfish.cs
+++ Assets/_Scripts/Pufferfish.cs
@@ -7,70 +7,79 @@ public class Pufferfish : MonoBehaviour, IDamageable
     [SerializeField] private LayerMask aimMask;
     [SerializeField] private float speed;
     [SerializeField] private float range;
+    [SerializeField] private Sprite belowSprite;
+    [SerializeField] private Sprite otherSprite;
 
     private GameObject player; // Change to singleton later
     private Rigidbody2D rb;
+    private Animator anim;
     private Vector2 dirToPlayer;
+    private SpriteRenderer sr;
 
     // Parameters touched by OnTriggerEnter
     private bool _CheckForPlayer = false;
     private bool _Expanding;
+    private bool _FacingBelow;
+    private bool _Expanded;
     
 
     void Awake() 
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponentInChildren<SpriteRenderer>();
+        anim = GetComponentInChildren<Animator>();
     }
 
     void Start() {
         player = PlayerController.Instance.gameObject;
-        // Debug.Log(player);
-        // Collider2D[] colList = player.GetComponentsInChildren<Collider2D>();
-        // foreach (Collider2D col in colList) {
-        //     Debug.Log(col);
-        // }
+        sr.sprite = otherSprite;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        
+        // Controlling rotation
+        gameObject.transform.eulerAngles = new Vector3(0, ((player.transform.position - transform.position).x < 0)? 0 : 180f, 0);
+        if (Mathf.Atan(dirToPlayer.y / Mathf.Abs(dirToPlayer.x)) * Mathf.Rad2Deg <  -45f) {
+            if (_Expanded) {
+                anim.Play("downBig");
+            } else {
+                anim.Play("down");
+            }
+            
+            // sr.sprite = belowSprite;
+        } else {
+            if (_Expanded) {
+                anim.Play("sideBig");
+            } else {
+                anim.Play("side");
+            }
+            // sr.sprite = otherSprite;
+        }
+
+        // Control Movement
+        dirToPlayer = player.transform.position - transform.position;
         _CheckForPlayer = (Vector3.Distance(player.transform.position, transform.position) <= range);
         if (_CheckForPlayer) {
-            // Debug.Log("Checking for player");
-            dirToPlayer = player.transform.position - transform.position;
+
             RaycastHit2D ray = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y), dirToPlayer, Mathf.Infinity, aimMask);
             // Debug.DrawRay(transform.position, dirToPlayer * 10);
             if (ray.collider != null && LayerMask.LayerToName(ray.collider.gameObject.layer).Equals("Player")) {
-                
                 MoveTowardsPlayer();
+                if (!_Expanding && (Vector3.Distance(player.transform.position, transform.position) <= 4)) {
+                    Expand();
+                }
             }
-            // // if (!_Expanding && Vector3.Distance(player.transform.position, transform.position) <= 2) {
-            //     _Expnding = true;
-            //     StartCoroutine(ExpandRoutine());
-            // } else {
-            //     StartCoroutine(Expand)
-            // }
-            // Debug.Log("The ray hit " + ray.collider.name);
 
-        } else {
-            // rb.linearVelocity = Vector2.zero;
+        } else if (_Expanding) {
+            Shrink();
         }
+
+        // Rotate GameObject
+         // If positive, then 
+        
+        // Debug.Log(Mathf.Atan(dirToPlayer.y / dirToPlayer.x)); 
     }
-
-    // void OnTriggerEnter2D(Collider2D other) {
-    //     // Debug.Log("Entered");
-    //     if (LayerMask.LayerToName(other.gameObject.layer).Equals("Player")) {
-    //         _CheckForPlayer = true;
-    //     }
-    // }
-
-    // void OnTriggerExit2D(Collider2D other) {
-    //     // Debug.Log("Exited");
-    //     if (LayerMask.LayerToName(other.gameObject.layer).Equals("Player")) {
-    //         _CheckForPlayer = false;
-    //     }
-    // }
 
     void FireRay() {
         Ray ray = new Ray(transform.position, transform.forward);
@@ -90,16 +99,18 @@ public class Pufferfish : MonoBehaviour, IDamageable
     }
 
     private void Expand() {
-        StartCoroutine(ExpandRoutine());
+        anim.Play("Expand");
+        _Expanding = true;
+        _Expanded = true;
     }
 
     private void Shrink() {
-        StartCoroutine(ShrinkRoutine());
-        transform.localScale = new Vector2(1,1);
+        anim.Play("Shrink");
+        _Expanding = false;
+        _Expanded = false;
     }
 
     private IEnumerator ExpandRoutine() {
-        // while ()
         yield return null;
     }
 
